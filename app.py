b1424d0790a28c32655762b07f986ca5e52d4b67
Jude Martin <judemartin.mj@gmail.com>
2023-05-23 20:06:25 -0500

    Refactor

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-from api.book import blueprint as book_app
-from api.page import blueprint as page_app
+from api.book_api import blueprint as book_api
+from api.page_api import blueprint as page_api
 
 
 def register_blueprints():
-    app.register_blueprint(book_app)
-    app.register_blueprint(page_app)
+    app.register_blueprint(book_api)
+    app.register_blueprint(page_api)
 
 
 app = Flask(__name__)
