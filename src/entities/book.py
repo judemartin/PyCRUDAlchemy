1da61ac53f18322d652fbfdabc9d968cb76d4203
Jude Martin <judemartin.mj@gmail.com>
2023-05-24 21:58:08 -0500

    refactor book entity

--- src/entities/book.py
+++ src/entities/book.py
@@ -1,4 +1,4 @@
-import sqlalchemy
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 
 from src.data.modelbase import SqlAlchemyBase
@@ -7,9 +7,9 @@ from src.data.modelbase import SqlAlchemyBase
 class Book(SqlAlchemyBase):
     __tablename__ = 'book'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    author = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    isbn = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=True)
+    author = Column(String, nullable=True)
+    description = Column(String, nullable=True)
+    isbn = Column(String, nullable=True)
     pages = relationship("Page", back_populates='book')
